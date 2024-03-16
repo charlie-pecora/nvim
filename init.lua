21855932b25553b6408ed508c7ac89232f68ff38
charlie-pecora <charlie.pecora@gmail.com>
2024-03-16 14:34:35 -0700

    set used language servers

--- init.lua
+++ init.lua
@@ -533,8 +533,10 @@ require('lazy').setup {
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        jedi_language_server = {},
         ruff_lsp = {},
+        mypy = {},
+        black = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
