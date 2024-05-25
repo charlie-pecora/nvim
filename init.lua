fbd646264370a08fa1091ec6d156d1cccedb76df
charlie-pecora <charlie.pecora@gmail.com>
2024-05-25 11:48:47 -0600

    configure linters

--- init.lua
+++ init.lua
@@ -637,6 +637,32 @@ require('lazy').setup {
   },
 ]]
   --
+{
+  "mfussenegger/nvim-lint",
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      python = { "ruff", "mpypy" },
+      go = { "golangcilint" },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+},
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
